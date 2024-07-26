b4c651cfe3bee957bab2bdf0a29fbeb4ca2abefd
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-07-26 11:16:44 +0530

    Conditionals In Python - Video 5

---
+++ tut4.py
@@ -0,0 +1,27 @@
+# Conditional Statements: if, elif, else
+
+# age = int(input("Enter your age: "))
+
+'''
+if (age < 18):
+    print("You are not eligible for driving!")
+else:
+    print("You are eligible for driving!")
+'''
+
+'''
+if (age < 18):
+    print("You are not eligible for driving!")
+elif (age >= 18 and age <= 24):
+    print("You are eligible for driving but drive safely!")
+else:
+    print("You are eligible for driving!")
+'''
+
+d = {1: 'Apple', 2: 'Orange', 3: 'Kiwi', 4: 'Guava', 5: 'Grapes'}
+
+if (6 in d):
+    print(d[6])
+else:
+    print('Key is not present!')
+
