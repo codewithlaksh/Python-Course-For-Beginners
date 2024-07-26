20bfbd2cfec42eb2f3df512e19d6618426671f1e
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-07-26 08:36:56 +0530

    Types Of Operators - Video 4

---
+++ tut3.py
@@ -0,0 +1,71 @@
+# Arithmetic Operators : +, -, *, /, **, //, %
+a = 3
+b = 4
+
+'''
+print('a+b =', a+b)
+print('a-b =', a-b)
+print('a*b =', a*b)
+print('a/b =', a/b)
+print('a**b =', a**b)
+print('a//b =', a//b)
+print('a%b =', a%b)
+'''
+
+# Assignment Operators : =, +=, -=, *=, /=, **=, //=, %=
+
+num1 = 900
+num2 = 700
+
+# print('num1 is', num1)
+
+num1 += num2
+# print(num1)
+num1 -= 100
+# print(num1)
+num1 *= 0.5
+# print(num1)
+num1 /= 2
+# print(num1)
+num1 **= 2
+# print(num1)
+num1 //= 5
+# print(num1)
+num1 %= 6
+# print(num1)
+
+# Logical Operators: and, or, not
+
+'''
+print('--- AND ---')
+
+print(True and True) # 1 (True)
+print(False and True) # 0 (False)
+print(True and False) # 0 (False)
+print(False and False) # 0 (False)
+
+print('--- OR ---')
+
+print(True or True) # 1 (True)
+print(False or True) # 1 (True)
+print(True or False) # 1 (True)
+print(False or False) # 0 (True)
+
+print('--- NOT ---')
+
+print(not True) # 0 (False)
+print(not False) # 1 (True)
+'''
+
+# Membership operators: in, not in
+'''
+print(2 in [1, 3, 4, 5])
+print(2 not in [1, 3, 4, 5])
+'''
+
+# Compraison Operators: ==, <=, >=, <, >
+print(9 == 10)
+print(9 <= 10)
+print(9 >= 10)
+print(9 < 10)
+print(9 > 10)
