ec06f11418be9916c97e2a382f2c43ef5c3145eb
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-07-29 09:52:32 +0530

    Quiz 1 Solution & First Exercise - Video 8

---
+++ quiz1.py
@@ -0,0 +1,20 @@
+# Reverse a string without using string slicing method
+
+string = "Python is my favourite programming language"
+new_string = ""
+
+'''
+for i in string:
+    new_string = i + new_string
+    print(new_string)
+'''
+
+'''
+i = 0
+while i < len(string):
+    new_string = string[i] + new_string
+    print(new_string)
+
+    i += 1
+'''
+
---
+++ tut7.py
@@ -0,0 +1,8 @@
+# Take a number as input from and check if the number is perfect number, armstrong number or palindrome
+'''
+Perfect Number: a number which is equal to the sum of its prime factors.
+Armstrong Number: a number which is equal to the sum of cubes of its digits
+Palindrome Number: a number which is equal to the reverse of that number
+'''
+
+'''----- ANSWER -----'''
