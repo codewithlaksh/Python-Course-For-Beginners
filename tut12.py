51a18c95b116d5c18e3dc39f36e37a6264d86c31
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-08-18 10:22:41 +0530

    Exercise 2: Generate Fibonacci Series - Video 14

---
+++ tut12.py
@@ -0,0 +1,12 @@
+'''
+Exercise 2: Generate n terms of a fibonacci series. Store them in a list. Display the list as well as the sum upto n terms of the series
+'''
+
+'''
+0, 1, 1, 2, 3, 5, 8 ..... n terms
+
+n (int) --> take input from the user
+'''
+
+lst = []
+s = 0
