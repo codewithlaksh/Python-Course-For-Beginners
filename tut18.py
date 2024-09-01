760d04afc98508b388acdd23a61c8e6db9079748
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-09-01 19:40:23 +0530

    File I/O : a & r+ modes in text files - Video 20

---
+++ tut18.py
@@ -0,0 +1,14 @@
+# f = open("demo.txt")
+# print(f.read())
+
+# f = open("demo.txt", "a")
+
+# f.write('\nI am a youtuber!!!')
+# f.writelines(['\nI am a python enthusiast.\n', 'I am a web developer as well!'])
+
+f = open("demo.txt", "r+")
+# f = open("laksh2.txt", "r+") FileNotFoundError
+
+# print(f.read())
+# print(f.readlines()[2].rstrip('\n'))
+# f.write('Thanks for watching this video & I will see you next time!')
